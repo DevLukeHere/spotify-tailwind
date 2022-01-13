328111a476b467eae9cc4ff948e47876dca0707d
Luke Wong <luke@wobbjobs.com>
2022-01-13 16:50:07 +0800

    Configured tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,18 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    fontFamily: {
-      inter: ['Inter', 'sans-serif']
-    },
-    colors: {
-      'spotify-green-primary': '#1DB954',
-      'spotify-green-secondary': '#1DB954',
-      'spotify-black': '#191414',
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif']
+      },
+      colors: {
+        'spotify-green-primary': '#1DB954',
+        'spotify-green-secondary': '#1DB954',
+        'spotify-black': '#191414',
+      },
+      gridTemplateColumns: {
+        'footerLeft': 'auto auto minmax(0, 1fr)',
+      },
     }
   },
   extend: {},
