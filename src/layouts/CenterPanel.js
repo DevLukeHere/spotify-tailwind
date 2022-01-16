3b0a12fcaf0950b3643f501244c1ed1fb55d3440
Luke Wong <luke@wobbjobs.com>
2022-01-16 23:21:42 +0800

    Added background colour gradient

--- src/layouts/CenterPanel.js
+++ src/layouts/CenterPanel.js
@@ -1,9 +1,5 @@
 function CenterPanel() {
-  return (
-    <div>
-      
-    </div>
-  );
+  return <div className="bg-gradient-to-t from-slate-900 to-slate-700"></div>;
 }
- 
+
 export default CenterPanel;
