3fb16bbcb9d95fe1b3806c9b2210fea8ee1a812f
Luke Wong <luke@wobbjobs.com>
2022-01-16 23:21:24 +0800

    Added title and add user icon

--- src/layouts/FriendActivity.js
+++ src/layouts/FriendActivity.js
@@ -1,9 +1,14 @@
-function FriendActivity () {
+import { UserAddIcon } from "@heroicons/react/outline";
+
+function FriendActivity() {
   return (
-    <div>
-      
+    <div className="p-4 bg-spotify-black">
+      <div className="text-white grid grid-cols-2">
+        <p className="font-bold">Friend activity</p>
+        <UserAddIcon className="w-5 h-5 place-self-end stroke-gray-400 hover:stroke-white" />
+      </div>
     </div>
   );
 }
- 
+
 export default FriendActivity;
