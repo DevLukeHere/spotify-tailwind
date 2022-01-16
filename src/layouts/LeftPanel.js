ffe65345417cd3fc4872baeea8d9e0ceff2d6253
Luke Wong <luke@wobbjobs.com>
2022-01-16 23:21:00 +0800

    Reduced icon size

--- src/layouts/LeftPanel.js
+++ src/layouts/LeftPanel.js
@@ -8,28 +8,28 @@ import {
 
 function LeftPanel() {
   return (
-    <div className="bg-spotify-black pt-8 pl-2 pb-2 pr-2 max-w-sm h-screen">
+    <div className="bg-spotify-black pt-8 pl-2 pb-2 pr-2 h-screen">
       <div className="mb-5">
         <div className="pl-4 pt-2 pb-2 rounded grid grid-cols-leftPanel gap-3 mb-3 cursor-pointer text-white stroke-white border-white bg-gray-600/50">
-          <HomeIcon className="w-7 h-7" />
+          <HomeIcon className="w-6 h-6" />
           <p className="self-center text-sm font-bold">Home</p>
         </div>
         <div className="pl-4 pt-2 pb-2 grid grid-cols-leftPanel gap-3 mb-3 cursor-pointer text-gray-400 hover:text-white hover:stroke-white">
-          <SearchIcon className="w-7 h-7" />
+          <SearchIcon className="w-6 h-6" />
           <p className="self-center text-sm font-semibold">Search</p>
         </div>
         <div className="pl-4 pt-2 pb-2 grid grid-cols-leftPanel gap-3 cursor-pointer text-gray-400 hover:text-white hover:stroke-white">
-          <LibraryIcon className="w-7 h-7" />
+          <LibraryIcon className="w-6 h-6" />
           <p className="self-center text-sm font-semibold">Library</p>
         </div>
       </div>
       <div className="border-b border-solid border-gray-600 mb-5">
         <div className="pl-4 grid grid-cols-leftPanel cursor-pointer gap-3 mb-3 text-gray-400 hover:text-white hover:stroke-white">
-          <PlusIcon className="w-7 h-7" />
+          <PlusIcon className="w-6 h-6" />
           <p className="self-center text-sm font-semibold">Create Playlist</p>
         </div>
         <div className="pl-4 grid grid-cols-leftPanel cursor-pointer gap-3 mb-3 text-gray-400 hover:text-white hover:stroke-white">
-          <HeartIcon className="w-7 h-7" />
+          <HeartIcon className="w-6 h-6" />
           <p className="self-center text-sm font-semibold">Liked Songs</p>
         </div>
       </div>
